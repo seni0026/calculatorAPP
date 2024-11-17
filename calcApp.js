1499cb2ce58bbf656229bdefb273974a70735791
Kevon <seni0026@algonquinlive.com>
2024-11-16 23:38:08 -0500

    save the calculation to local storage

--- calcApp.js
+++ calcApp.js
@@ -1,6 +1,11 @@
-let calculation = '';
+// load calculation from the local storage into the calculation variable
+let calculation = localStorage.getItem('calculation') || '';
 
 function updateCalculation (btn) {
     calculation += btn;
     console.log(calculation);
+
+    // save calculation to local storage
+    localStorage.setItem('calculation', JSON.stringify(calculation));
 }
+
