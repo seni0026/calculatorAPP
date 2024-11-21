d200116e160787bec8de44256f5bd09ac5c409c9
Kevon <seni0026@algonquinlive.com>
2024-11-20 20:18:05 -0500

    add css

--- calcApp.js
+++ calcApp.js
@@ -1,11 +1,18 @@
 // load calculation from the local storage into the calculation variable
 let calculation = localStorage.getItem('calculation') || '';
+// updateCalculation ('');
+showCalculation();
 
 function updateCalculation (btn) {
     calculation += btn;
-    console.log(calculation);
+
+    showCalculation();
 
     // save calculation to local storage
-    localStorage.setItem('calculation', JSON.stringify(calculation));
+    localStorage.setItem('calculation', calculation);
 }
 
+// create a function to display the calculations
+function showCalculation() {
+    document.querySelector('.js-calculate').innerHTML = calculation;
+}
